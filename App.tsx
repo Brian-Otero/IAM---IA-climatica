9ff7265208d8d01c20fdf0a7cd7d647a4ba274c5
Brian Otero <brian.otero0284@alumnos.udg.mx>
2024-09-14 20:03:35 -0600

    - Comentarios extra en APP.tsx

--- App.tsx
+++ App.tsx
@@ -8,9 +8,17 @@
 import React from 'react';
 import Menu from './components/Menu/Menu';
 import Mapa from './components/Mapa/Mapa'
+
+
+
 function App(): React.JSX.Element {
   return (
+    /*Cambiar segun sea necesario 
+    <Menu></Menu>
+    */
     <Mapa></Mapa>
+
+    //La navegacion esta pendiente
   );
 }
 
