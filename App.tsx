2fb93366a97194f151d6b2ff19ec4558d193118b
Brian Otero <brian.otero0284@alumnos.udg.mx>
2024-09-27 16:09:07 -0600

    - Se agrego la navegacion principal a la app

--- App.tsx
+++ App.tsx
@@ -1,28 +1,27 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import 'react-native-gesture-handler';
 import Menu from './components/Menu/Menu';
-import Mapa from './components/Mapa/Mapa'
+import Mapa from './components/Mapa/Mapa';
 
 
+type RootStackParamList = {
+  Menu: undefined;
+  Mapa: undefined;
+};
+
+const Stack = createStackNavigator<RootStackParamList>();
 
 function App(): React.JSX.Element {
   return (
-    /*Cambiar segun sea necesario 
-    <Menu></Menu>
-    */
-    // <Mapa></Mapa>
-    <Menu></Menu>
-
-    //La navegacion esta pendiente
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Menu" screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Menu" component={Menu} />
+        <Stack.Screen name="Mapa" component={Mapa} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 export default App;
-
-
