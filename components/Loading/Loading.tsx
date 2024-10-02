9be152f67f1699a8fde66890165cc7346c83a34b
Brian-Otero <thecrafter.2030@gmail.com>
2024-10-01 22:44:15 -0600

    Revert "Update Logo IAM"
    This reverts commit 461b8fc808b710d8576690b26fbed4d78e5b5dbe.

--- components/Loading/Loading.tsx
+++ components/Loading/Loading.tsx
@@ -5,8 +5,8 @@ const Loading: React.FC = () => {
     return (
         <View style={styles.container}>
             <Image
-                source={require('./assets/logo_iam_m20_oarp.png')}
-                style={styles.icon}
+                source={require('./assets/cloud-icon.png')}
+                style={styles.cloudIcon}
             />
             
             <ActivityIndicator size="large" color="#4f5f96" />
@@ -21,9 +21,9 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         backgroundColor: '#abb5ed',
     },
-    icon: {
-        width: 300,  
-        height: 300,
+    cloudIcon: {
+        width: 100,  
+        height: 100,
         marginBottom: 20,
     },
 })
