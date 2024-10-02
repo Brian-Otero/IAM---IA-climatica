fd044a45f8c2c693539fec1a9f6827dcba60c405
Brian-Otero <thecrafter.2030@gmail.com>
2024-10-01 22:42:22 -0600

    Revert "Update"
    This reverts commit ec09a5900621439b5c1aa3e789bffe440b37fdd7.

--- components/Menu/Menu.tsx
+++ components/Menu/Menu.tsx
@@ -69,8 +69,8 @@ const styles = StyleSheet.create({
     },
 
     logo: {
-        width: 90,
-        height: 90,
+        width: 60,
+        height: 60,
         resizeMode: 'contain',
         marginRight: 5,
     },
