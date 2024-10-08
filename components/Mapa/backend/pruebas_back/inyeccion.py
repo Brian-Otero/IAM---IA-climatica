eeb6232965d05204598eae9dffe510e7974f8e11
Brian-Otero <thecrafter.2030@gmail.com>
2024-10-08 02:04:00 -0600

    - se agrega programa para realizar pruebas de inyeccion a la db

---
+++ components/Mapa/backend/pruebas_back/inyeccion.py
@@ -0,0 +1,20 @@
+import os
+import requests
+
+# URL de la API
+url = "http://148.202.152.59:8002/upload/"
+
+# Ruta de la carpeta con los archivos KML
+kml_folder = "/ruta/a/tu/carpeta/kmls"
+
+# Iterar sobre todos los archivos KML en la carpeta
+for filename in os.listdir(kml_folder):
+    if filename.endswith(".kml"):  # Verificar que el archivo sea KML
+        file_path = os.path.join(kml_folder, filename)
+
+        # Enviar el archivo a la API usando una petición POST
+        with open(file_path, 'rb') as file:
+            response = requests.post(url, files={"file": file})
+
+        # Imprimir la respuesta
+        print(f"Archivo {filename} procesado: {response.status_code} - {response.json()}")
